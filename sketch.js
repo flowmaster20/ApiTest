e699cf127272ba400b0158dacc1e8d94838a091a
flowmaster20 <31287387+flowmaster20@users.noreply.github.com>
2018-02-13 21:28:51 +0100

    cosmetic changes

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 function preload() {
   var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
   earthquakes = loadJSON(url);
@@ -6,7 +5,7 @@ function preload() {
 
 function setup() {
 
-  createCanvas(360*4, 180*4);
+  createCanvas(360 * 4, 180 * 4);
   background(255);
 }
 
@@ -17,15 +16,15 @@ function draw() {
   var mag = earthquakes.features[0].properties.mag;
   var name = earthquakes.features[0].properties.title;
   var depth = round(earthquakes.features[1].geometry.coordinates[2])
-var c = color(map(depth,0,100,0,200));
+  var c = color(map(depth, 0, 100, 0, 200));
   fill(c);
-  translate(180*4, 90*4);
+  translate(180 * 4, 90 * 4);
   rect(0, 0, 10, 10);
-   fill(c);
-  ellipse(long*4, -lat*4, 20*mag, 20*mag);
-  text(name, long*4,-lat*4-20);
-  for(var i = 0;i<100000000;i++){
-  var r = 100000000 - i;
+  fill(c);
+  ellipse(long * 4, -lat * 4, 20 * mag, 20 * mag);
+  text(name, long * 4, -lat * 4 - 20);
+  for (var i = 0; i < 100000000; i++) {
+    var r = 100000000 - i;
     var tekst = i;
   }
   location.reload(true);
